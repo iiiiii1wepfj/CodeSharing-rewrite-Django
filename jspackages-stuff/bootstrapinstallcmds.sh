c51b72ca6ad7dc638a4e6f78b55ff9c922d4364f
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2024-08-12 10:12:12 +0300

    upgrade django

--- jspackages-stuff/bootstrapinstallcmds.sh
+++ jspackages-stuff/bootstrapinstallcmds.sh
@@ -1,3 +1,3 @@
-npm install bootstrap@latest
+npm install bootstrap@v5.3.3
 cp node_modules/bootstrap/dist/css/bootstrap.min.css ../CodeSharing/App/static/css/
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ../CodeSharing/App/static/js/
